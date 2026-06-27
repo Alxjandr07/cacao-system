9b8b98b117667a361f965f50b17dd3c9a4bfe87a
Alxjandr07 <luistejada5434@gmail.com>
2026-06-26 22:18:46 -0500

    feat: configuracion inicial del proyecto Spring Boot

---
+++ src/main/java/com/caco/cacao_system/CacaoSystemApplication.java
@@ -0,0 +1,13 @@
+package com.caco.cacao_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaoSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacaoSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caco/cacao_system/CacaoSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caco.cacao_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaoSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
