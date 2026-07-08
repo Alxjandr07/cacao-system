ea7315c884625214e517af7a8d0b309953d4ed93
Alxjandr07 <luistejada5434@gmail.com>
2026-07-07 23:06:12 -0500

    feat: configuracion de Spring Security

--- src/main/java/com/caco/cacao_system/repository/RolRepository.java
+++ src/main/java/com/caco/cacao_system/repository/RolRepository.java
@@ -1,5 +1,11 @@
 package com.caco.cacao_system.repository;
 
-public class RolRepository {
+import com.caco.cacao_system.model.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByNombre(String nombre);
 }
